0c31a52b051b5f159964fba6bd5bcaef2b8be129
Paolo DePetrillo <paolod@gmail.com>
2023-12-04 00:31:26 -0500

    Unpack vkdt files in local storage

--- app/src/main/cpp/gen_vkdtbase.sh
+++ app/src/main/cpp/gen_vkdtbase.sh
@@ -3,18 +3,19 @@ shopt -s globstar
 
 ZIPFILE=$1
 ROOT=$2
-MODS=src/pipe/modules/*
 
 cd $ROOT
 rm $ZIPFILE
 zip -9r $ZIPFILE \
   bin/data \
-  bin/default* \
-  $MODS/params \
-  $MODS/connectors \
-  $MODS/*.ui \
-  $MODS/ptooltips \
-  $MODS/ctooltips \
-  $MODS/readme.md \
-  $MODS/*.so \
-  src/pipe/modules/**.spv
+  bin/default*
+cd src/pipe
+zip -9r $ZIPFILE \
+  modules/*/params \
+  modules/*/connectors \
+  modules/*/*.ui \
+  modules/*/ptooltips \
+  modules/*/ctooltips \
+  modules/*/readme.md \
+  modules/*/*.so \
+  modules/**.spv
---
+++ app/src/main/java/com/github/paolodepetrillo/vkdtandroidtest/VkdtBase.kt
@@ -0,0 +1,49 @@
+package com.github.paolodepetrillo.vkdtandroidtest
+
+import android.content.Context
+import java.io.File
+import java.lang.RuntimeException
+import java.util.zip.ZipInputStream
+
+class VkdtBase(context: Context) {
+    val root = File(context.filesDir, "vkdtbase")
+    private val flagFile = File(root, "_unpacked_")
+
+    init {
+        if (!flagFile.exists()) {
+            unpack(context)
+        }
+    }
+
+    private fun unpack(context: Context) {
+        if (root.exists()) {
+            root.deleteRecursively()
+        }
+        context.assets.open("vkdtbase.zip").use { ins ->
+            ZipInputStream(ins).use { zip ->
+                generateSequence { zip.nextEntry }
+                    .filterNot { it.isDirectory }
+                    .forEach { entry ->
+                        val dest = File(root, entry.name)
+                        dest.parentFile?.let {
+                            if (!it.exists()) {
+                                if (!it.mkdirs()) {
+                                    throw RuntimeException("Failed to create directory")
+                                }
+                            }
+                        }
+                        dest.outputStream().buffered().use { bos ->
+                            val buf = ByteArray(4096)
+                            while (zip.available() == 1) {
+                                val size = zip.read(buf)
+                                bos.write(buf, 0, size)
+                            }
+                        }
+                    }
+            }
+        }
+        flagFile.outputStream().use {
+            it.write(0)
+        }
+    }
+}
