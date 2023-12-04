f4ffdca6d2550b14134f85b4577b4e3c6488b6b4
Paolo DePetrillo <paolod@gmail.com>
2023-12-03 23:37:35 -0500

    Init Vulkan

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,16 @@
 #include <jni.h>
 #include <string>
 
+extern "C" {
+#include "qvk/qvk.h"
+#include "pipe/global.h"
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_github_paolodepetrillo_vkdtandroidtest_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    VkResult res = qvk_init(0, -1);
+    std::string hello = "Hello from C++ " + std::to_string(res);
     return env->NewStringUTF(hello.c_str());
 }
